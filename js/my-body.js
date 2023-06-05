053db6b516013bf0256bc1eee690dbf0f16567f3
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-06-05 08:09:18 -0500

    feat: :sparkles: Función del webComponent my-body
    Se realizó la funcion de que sumaran los valores o restaran para ademas de eliminar las filas

--- js/my-body.js
+++ js/my-body.js
@@ -1,16 +1,39 @@
 import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" assert { type: "css" };
-export class myBody extends HTMLElement{
-    constructor(){
+export class myBody extends HTMLElement {
+    constructor() {
         super();
+        document.adoptedStyleSheets.push(styles);
     }
-    async components(){
+    async components() {
         return await (await fetch("view/my-body.html")).text();
     }
-    connectedCallback(){
-        document.adoptedStyleSheets.push(styles);
-        this.components().then(html=>{
+    selection(e) {
+        let $ = e.target;
+        if ($.nodeName == "BUTTON") {
+            let inputs = document.querySelectorAll(`#${$.dataset.row} input`);
+            if ($.innerHTML == "-") {
+                inputs.forEach(element => {
+                    if (element.name == "amount" && element.value == 0) {
+                        document.querySelector(`#${$.dataset.row}`).remove();
+                    } else if (element.name == "amount") {
+                        element.value--;
+                    }
+                });
+            } else if ($.innerHTML == "+") {
+                inputs.forEach(element => {
+                    if (element.name == "amount") {
+                        element.value++;
+                    }
+                });
+            }
+        }
+    }
+    connectedCallback() {
+        this.components().then(html => {
             this.innerHTML = html;
+            this.container = this.querySelector("#products");
+            this.container.addEventListener("click", this.selection);
         })
     }
 }
-customElements.define('my-body',myBody);
+customElements.define('my-body', myBody);
