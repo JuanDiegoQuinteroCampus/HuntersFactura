769e2b7dc04f2080022a53dca3b0186ee2bc313f
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-06-21 08:38:31 -0500

    feat: :sparkles: CRUD seller.php
    Se realizó el crud de seller.php, que permite enviar y obtener la data de la tabla

--- scripts/db/credentials.php
+++ scripts/db/credentials.php
@@ -1,6 +1,6 @@
 <?php
     abstract class credentials{
-    protected $host = '172.16.48.210';
+    protected $host = '172.16.48.204';
     private $user = 'sputnik';
     private $password = 'Sp3tn1kC@';
     protected $dbname = 'db_hunter_facture_juanquintero';
--- scripts/seller/seller.php
+++ scripts/seller/seller.php
@@ -1,7 +1,33 @@
 <?php
-class Seller{
+class seller extends connect{
+    private $queryPost = 'INSERT INTO tb_seller(id_seller,name_seller) VALUES(:id,:seller)';
+    private $queryGetAll = 'SELECT id_seller AS "id", name_seller AS "seller" FROM tb_seller';
+    private $message;
     use getInstance;
-    function __construct(public $Seller){}
-    
+    function __construct(public $Name_Seller =3, private $id_seller =3){parent::__construct();}
+    public function postSeller(){
+        try {
+            $res = $this->conx->prepare($this->queryPost);
+            $res->bindValue("id", $this->id_seller);
+            $res->bindValue("seller", $this->Name_Seller);
+            $res->execute();
+            $this->message = ["Code"=> 200+$res->rowCount(), "Message"=> "inserted data3"];
+        } catch(\PDOException $e) {
+            $this->message = ["Code"=> $e->getCode(), "Message"=> $res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
+    public function getAllSeller(){
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $this->message = ["Code"=> 200, "Message"=> $res->fetchAll(PDO::FETCH_ASSOC)];
+        } catch(\PDOException $e) {
+            $this->message = ["Code"=> $e->getCode(), "Message"=> $res->errorInfo()[2]];
+        }finally{
+            print_r($this->message);
+        }
+    }
 }
-?>
+?>  
